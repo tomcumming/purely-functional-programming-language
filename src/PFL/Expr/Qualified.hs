f5b76ab63dafb66dc5da7bb8edb4f51e4da3826f
Tom Cumming <mail@tomcumming.co.uk>
2025-07-21 22:26:08 +0100

    Revert "No more local names"
    This reverts commit d4f47e6bf585c4835ff1a5dff1e4f547cf7be37a.

--- src/PFL/Expr/Qualified.hs
+++ src/PFL/Expr/Qualified.hs
@@ -1,12 +1,4 @@
-module PFL.Expr.Qualified
-  ( Expr (..),
-    Di,
-    VarCount,
-    unAbs,
-    unAbsCount,
-    free,
-  )
-where
+module PFL.Expr.Qualified (Expr (..), free) where
 
 import Control.Category ((>>>))
 import Control.Comonad.Cofree qualified as CF
@@ -19,33 +11,18 @@ import Data.Set qualified as S
 import Data.Text qualified as T
 import GHC.Generics (Generic1)
 
--- | Debruijn index
-newtype Di = Di Int
-  deriving newtype (Eq, Ord, Show, Enum)
-
-newtype VarCount = VarCount Int
-  deriving newtype (Eq, Ord, Show, Enum)
-
-unAbs :: S.Set Di -> S.Set Di
-unAbs = S.filter (>= toEnum 0) . S.mapMonotonic pred
-
-unAbsCount :: VarCount -> S.Set Di -> S.Set Di
-unAbsCount c xs
-  | c <= toEnum 0 = xs
-  | otherwise = unAbsCount (pred c) (unAbs xs)
-
 data Expr a
-  = Local Di
+  = Local T.Text
   | Global T.Text
-  | Abs a
+  | Abs T.Text a
   | Ap a a
   deriving (Eq, Ord, Show, Functor, Foldable, Generic1)
   deriving (Show1) via FunctorClassesDefault Expr
 
-free :: CF.Cofree Expr ann -> S.Set Di
+free :: CF.Cofree Expr ann -> S.Set T.Text
 free =
   cata $
     tailF >>> \case
       Local x -> S.singleton x
-      Abs xs -> S.filter (>= toEnum 0) $ S.map pred xs
+      Abs x xs -> S.delete x xs
       e -> fold e
