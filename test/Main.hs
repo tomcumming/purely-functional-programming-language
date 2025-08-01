45b209e8f59e12472e9014fd97cd591c9d4d1a9f
Tom Cumming <mail@tomcumming.co.uk>
2025-08-01 14:51:57 +0100

    Use tasty

--- test/Main.hs
+++ test/Main.hs
@@ -2,64 +2,66 @@ module Main (main) where
 
 import Control.Comonad.Cofree qualified as CF
 import Data.Text qualified as T
-import Data.Text.IO qualified as T
 import PFL.Compile.Linearise qualified as Linearise
 import PFL.Compile.Qualify (qualify)
 import PFL.Compile.Thread qualified as Thread
 import PFL.Expr.In qualified as In
 import PFL.Expr.Qualified qualified as Q
 import Test.Sexp qualified as Sexp
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.HUnit (testCase)
 
 type InExpr = CF.Cofree In.Expr ()
 
 type QExpr = CF.Cofree (Q.Expr T.Text T.Text) ()
 
 main :: IO ()
-main = do
-  testQualify
-  testThread
-  testLinearise
+main =
+  defaultMain $
+    testGroup
+      "PFL"
+      [ testQualify,
+        testThread,
+        testLinearise
+      ]
 
-testQualify :: IO ()
-testQualify = do
-  -- TODO many more!
-  T.putStrLn "Testing Qualify..."
-  doTest
-    "id"
-    "(abs x x)"
-    "(abs x (local x))"
-  doTest
-    "const global"
-    "(abs x y)"
-    "(abs x (global y))"
-  doTest
-    "simple app"
-    "(f x)"
-    "((global f) (global x))"
+testQualify :: TestTree
+testQualify =
+  testGroup
+    "Qualify"
+    [ testExpected
+        "id"
+        "(abs x x)"
+        "(abs x (local x))",
+      testExpected
+        "const global"
+        "(abs x y)"
+        "(abs x (global y))",
+      testExpected
+        "simple app"
+        "(f x)"
+        "((global f) (global x))"
+    ]
   where
-    doTest :: T.Text -> T.Text -> T.Text -> IO ()
-    doTest name inptStr expectedStr = do
-      T.putStr $ "  Testing " <> name <> "... "
+    testExpected name inptStr expectedStr = testCase name $ do
       inpt :: InExpr <- Sexp.parseFail inptStr
       expected :: QExpr <- Sexp.parseFail expectedStr
       let outpt = qualify inpt
       case Sexp.unify (Sexp.into outpt) (Sexp.into expected) of
-        Right () -> T.putStrLn "OK"
-        Left err -> do
-          T.putStrLn "FAIL"
-          T.putStrLn err
+        Right () -> pure ()
+        Left err -> fail (T.unpack err)
 
-testThread :: IO ()
-testThread = do
-  T.putStrLn "Testing Thread..."
-  doTest
-    "simplest"
-    "(threaded (! mx))"
-    "(abs 0x ((global mx) (local 0x)))"
+testThread :: TestTree
+testThread =
+  testGroup
+    "Thread"
+    [ testExpected
+        "simplest"
+        "(threaded (! mx))"
+        "(abs 0x ((global mx) (local 0x)))"
+    ]
   where
-    doTest :: T.Text -> T.Text -> T.Text -> IO ()
-    doTest name inptStr expectedStr = do
-      T.putStr $ "  Testing " <> name <> "... "
+    testExpected name inptStr expectedStr = testCase name $ do
       inpt :: InExpr <- Sexp.parseFail inptStr
       expected :: QExpr <- Sexp.parseFail expectedStr
       let nms =
@@ -70,34 +72,32 @@ testThread = do
               }
       let outpt = Thread.thread nms $ Q.anonymise $ qualify inpt
       case Sexp.unify (Sexp.into outpt) (Sexp.into expected) of
-        Right () -> T.putStrLn "OK"
-        Left err -> do
-          T.putStrLn "FAIL"
-          T.putStrLn err
+        Right () -> pure ()
+        Left err -> fail (T.unpack err)
 
-testLinearise :: IO ()
-testLinearise = do
-  T.putStrLn "Testing Linearise..."
-  doTest
-    "Simplest copy"
-    "((local x) (local x))"
-    $ T.unlines
-      [ "(match ((global copy) (local x))",
-        "  ( (just Pair) (x 0x) ((local 0x) (local x)) )",
-        ")"
-      ]
-  doTest
-    "Simplest drop"
-    "(abs x (global y))"
-    $ T.unlines
-      [ "(abs x (match ((global drop) (local x))",
-        "  ( (just Unit) () (global y) )",
-        "))"
-      ]
+testLinearise :: TestTree
+testLinearise =
+  testGroup
+    "Linearise"
+    [ testExpected
+        "Simplest copy"
+        "((local x) (local x))"
+        $ T.unlines
+          [ "(match ((global copy) (local x))",
+            "  ( (just Pair) (x 0x) ((local 0x) (local x)) )",
+            ")"
+          ],
+      testExpected
+        "Simplest drop"
+        "(abs x (global y))"
+        $ T.unlines
+          [ "(abs x (match ((global drop) (local x))",
+            "  ( (just Unit) () (global y) )",
+            "))"
+          ]
+    ]
   where
-    doTest :: T.Text -> T.Text -> T.Text -> IO ()
-    doTest name inptStr expectedStr = do
-      T.putStr $ "  Testing " <> name <> "... "
+    testExpected name inptStr expectedStr = testCase name $ do
       inpt :: QExpr <- Sexp.parseFail inptStr
       expected :: QExpr <- Sexp.parseFail expectedStr
       let nms =
@@ -109,7 +109,5 @@ testLinearise = do
               }
       let outpt = Linearise.linearise nms $ Q.anonymise inpt
       case Sexp.unify (Sexp.into outpt) (Sexp.into expected) of
-        Right () -> T.putStrLn "OK"
-        Left err -> do
-          T.putStrLn "FAIL"
-          T.putStrLn err
+        Right () -> pure ()
+        Left err -> fail (T.unpack err)
